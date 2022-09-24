e72717dd363ec27f97dbc09c7e32db61c949818e
KrutiyV <krutiyvladislav@gmail.com>
2022-09-24 18:58:48 +0300

    homework

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+using InternetShop;
+
+namespace TestProject
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void test1()
+        {
+            Coconut coconut = new Coconut();
+            string result = coconut.ToString();
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void test2()
+        {
+            Coconut coconut = new Coconut();
+            string result = coconut.ToString();
+            Assert.Contains("Coconut", result);
+        }
+    }
+}
